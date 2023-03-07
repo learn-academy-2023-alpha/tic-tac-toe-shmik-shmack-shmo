5d12e6584553c1c9335c529594912a4471c30e4a
cmnorrisii <cmnorrisjr@gmail.com>
2023-03-07 10:01:59 -0800

    added a starting grid for the game

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react'
-import Square from './components/Square'
-import './App.css'
+import React, { useState } from "react";
+import Square from "./components/Square";
+import "./App.css";
 
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
   return (
     <>
-      <h1>Tic Tac Toe</h1>
-      <Square />
+      <h1 className="header">Shmik Shmack Shmo</h1>
+      <div className="gameboard">
+        {squares.map((value, index) => {
+          return <Square />;
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
