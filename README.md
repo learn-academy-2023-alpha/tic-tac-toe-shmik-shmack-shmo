94216722df851f2eb19e1f08e62bf4de18986b98
JoelCarr619 <JoelCarr619@gmail.com>
2023-03-07 10:31:53 -0800

    add the functionalilty to place marker in box upon click

--- README.md
+++ README.md
@@ -16,6 +16,11 @@ When creating a project it is important to think about organization of your code
   -center content on page
 
 - As a user, I can click on a square to mark it.
+
+ ✅ branch click-square
+  -set up onclick event for Square
+  -onclick will trigger a function that will place a symbol or emoji in the box clicked.
+
 - As a user, my partner can click on a square after me and see their mark.
 - As a user, I can't click on a square that has already been marked.
 - As a user, when either my partner or I win the game (three squares in a row: horizontally, vertically, or diagonally) I can see a notice telling me which player won.
--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React, { useState } from "react";
 import Square from "./components/Square";
 import "./App.css";
 
+
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const handleGamePlay = (index) => {
+    let updatedSquares = [...squares]
+    updatedSquares[index]= "Ⓧ"
+    setSquares(updatedSquares)
+
+
+  }
 
   return (
     <>
       <h1 className="header">Shmik Shmack Shmo</h1>
       <div className="gameboard">
         {squares.map((value, index) => {
-          return <Square />;
+          return <Square 
+          value={value}
+          key={index}
+          index={index}
+          handleGamePlay={handleGamePlay}
+          
+          />;
+          
         })}
       </div>
     </>
